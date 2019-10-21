f3515517b4de0e3037a675f98b18d1cb974a7424
Bykov Denis Stanislavovich <stivenrain@gmail.com>
2019-10-21 23:16:36 +0300

    tab added

--- src/components/tabs/index.jsx
+++ src/components/tabs/index.jsx
@@ -1,21 +1,46 @@
 import React from 'react';
+import cn from 'classnames';
+import PropTypes from 'prop-types';
 
-function Tabs() {
+function Tabs(props) {
+
+  const { tabActive } = props;
+
+  const overviewStyles = cn({
+    'movie-nav__item': true,
+    'movie-nav__item--active': !tabActive,
+  });
+
+  const overviewDetails = cn({
+    'movie-nav__item': true,
+    'movie-nav__item--active': tabActive === 1,
+  });
+
+  const overviewReviews = cn({
+    'movie-nav__item': true,
+    'movie-nav__item--active': tabActive === 2,
+  });
+
+  const tabActiveChangeHandler = evt => {
+    const element = evt.target;
+    props.onTabActiveChange(parseInt(element.dataset.name, 10));
+  }
+
   return (
     <nav className="movie-nav movie-card__nav">
       <ul className="movie-nav__list">
-        <li className="movie-nav__item">
-          <a href="#" className="movie-nav__link">
+        <li className={overviewStyles}>
+          <a data-name="0" href="#" className="movie-nav__link" onClick={tabActiveChangeHandler}>
             Overview
           </a>
         </li>
-        <li className="movie-nav__item movie-nav__item--active">
-          <a href="#" className="movie-nav__link">
+        <li className={overviewDetails}>
+          <a data-name="1" href="#" className="movie-nav__link" onClick={tabActiveChangeHandler}>
             Details
           </a>
         </li>
-        <li className="movie-nav__item">
-          <a href="#" className="movie-nav__link">
+        <li className={overviewReviews}>
+          <a data-name="2" href="#" className="movie-nav__link" onClick={tabActiveChangeHandler}>
             Reviews
           </a>
         </li>
@@ -23,5 +48,14 @@ function Tabs() {
     </nav>
   );
 }
+
+Tabs.defaultProps = {
+  tabActive: 0,
+};
+
+Tabs.propTypes = {
+  tabActive: PropTypes.number.isRequired,
+  onTabActiveChange: PropTypes.func.isRequired,
+};
 
 export default Tabs;
